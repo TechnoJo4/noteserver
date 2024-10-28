b80b519ce13d0f6bd8dd86095e4ecbb7a429a803
TechnoJo4 <technojo4@gmail.com>
2024-10-27 22:04:03 -0400

    Improve editor styling

--- src/client.ts
+++ src/client.ts
@@ -50,6 +50,7 @@ const openEditor = async () => {
     editor = new EditorView({
         doc: contents,
         extensions: [
+            EditorView.lineWrapping,
             keymap.of(defaultKeymap),
             lineNumbers(),
             drawSelection(),
